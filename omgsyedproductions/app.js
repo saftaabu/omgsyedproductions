d7f9b7f5f633b12edb510777b9a995507c4d4a5d
Syed Aftaabuddin <syedaftab299@gmail.com>
2020-01-23 20:02:53 -0600

    updated contact

--- omgsyedproductions/app.js
+++ omgsyedproductions/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
-var aboutRouter = require('./routes/contact');
+var contactRouter = require('./routes/contact');
 var app = express();
 
 // view engine setup
@@ -24,6 +24,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
