e2bc71282cc762ba9983c35e7db8d43a05afb868
Syed Aftaabuddin <syedaftab299@gmail.com>
2020-01-30 20:11:14 -0600

    updated one!

--- omgsyedproductions/app.js
+++ omgsyedproductions/app.js
@@ -4,10 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var aboutRouter = require('./routes/about');
 var contactRouter = require('./routes/contact');
+var layouts = require('express-ejs-layouts');
+
 var app = express();
 
 // view engine setup
@@ -19,11 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(layouts);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/about', aboutRouter);
 app.use('/contact', contactRouter);
+
 
 
 // catch 404 and forward to error handler
